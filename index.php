38df71207df100092ae5e3ac6e796652c7f64776
Persianscript <Persianscript@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-19 07:59:41 +0000

    version 4.0.0
    git-svn-id: https://plugins.svn.wordpress.org/payline-woocommerce/trunk@1138422 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: درگاه پرداخت پی لاین افزونه ووکامرس
+Version: 4.0.0
+Description: پرداخت وجه محصولات ووکامرس به وسیله درگاه پرداخت پی لاین
+Plugin URI: http://woocommerce.ir/
+Author: ووکامرس فارسی
+Author URI: http://woocommerce.ir/
+*/
+include_once("class-wc-gateway-payline.php");
+//By HANNANStd in PersianScript.ir ==> woocommerce.ir
